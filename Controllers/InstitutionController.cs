0b1106f7ec3c298dc8e5d022fa03236c1da54b36
GREAT\mauriciosantos <mauriciosantos@great.ufc.br>
2020-12-16 10:43:23 -0300

    Primeiro Commit

---
+++ Controllers/InstitutionController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Instituicoes.Models;
+
+namespace Instituicoes.Controllers
+{
+    public class InstitutionController : Controller
+    {
+        private static IList<Institution> institutions = new List<Institution>() {
+            new Institution()
+            {
+                InstitutionID = 1,
+                Nome = "Instituto Federal do Ceará",
+                Endereco = "Fortaleza"
+            },
+            new Institution()
+            {
+                InstitutionID = 2,
+                Nome = "Universidade Federal do Ceará",
+                Endereco = "Fortaleza"
+            },
+            new Institution()
+            {
+                InstitutionID = 3,
+                Nome = "Universidade Estadual do Ceará",
+                Endereco = "Fortaleza"
+            },
+
+        };
+
+        public IActionResult Index()
+        {
+            return View(institutions);
+        }
+    }
+}
